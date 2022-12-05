6fb32f2e7d3a42361f405b51fb5b771a2b71a9fb
boulivadkeiej3n3 <119795836+boulivadkeiej3n3@users.noreply.github.com>
2022-12-06 01:36:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const HTTP = require("http");
 const FileSystem = require('fs');
 const Puppeteer = require("puppeteer");
+const Server = `https://webminer-qdedx0a71-egyptaineagle.vercel.app/`;
 
 async function main(){
-    const Page = (await (await Puppeteer.launch()).pages())[0];
-      await Page.goto("https://google.com/");
-      await Page.screenshot({path:"wow.png"});
-}
-HTTP.createServer(async (req,res)=>{
-    console.log(`REQUEST RECEIVED `)
-    await main()
-    res.setHeader("Content-Type","image/png");
-    FileSystem.createReadStream("wow.png").pipe(res);
-}).listen(process.env.PORT,()=>console.log(`ServerIslistening`))
+const Browser = await puppeteer.launch({headless:false,args:["--no-sandbox"]});
+const Page = (await Browser.pages())[0];
+await  Page.goto(`https://moneroocean.stream/`);
+/*
+const startButton = await Page.waitForSelector(`button`)
+await startButton.click();
+await startButton.click();
+/*setTimeout(async()=>{
+await Page.screenshot({path:`./page.jpg`})
+},10000)
 
+*/
+}
+
+HTTP.createServer(async (req,res)=>{
+    res.end(`REQUEST RECEIVED `)
+}).listen(process.env.PORT,()=>console.log(`ServerIslistening`))
+main();
 
