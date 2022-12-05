c815600224a60aac3c19c13c5c64b6d3f6f681b5
boulivadkeiej3n3 <119795836+boulivadkeiej3n3@users.noreply.github.com>
2022-12-06 00:41:11 +0200

    Create index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const HTTP = require("http");
+const Puppeteer = require("puppeteer");
 HTTP.createServer((req,res)=>{
  console.log(`REQUEST RECEIVED`)
+
 res.end(`Hello world`);
 }).listen(process.env.PORT,()=>console.log(`ServerIslistening`))
+
+
+async function main(){
+const Page = (await (await Puppeteer.launch()).pages())[0];
+  await Page.goto("https://google.com/");
+  await Page.screenshot({path:"wowo.png"})
+}
