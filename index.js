9d6faccfa9a7a5abd429a2b26384406df7d6df21
boulivadkeiej3n3 <119795836+boulivadkeiej3n3@users.noreply.github.com>
2022-12-05 23:31:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const HTTP = require("http")
 HTTP.createServer((req,res)=>{
  console.log(`REQUEST RECEIVED`)
 res.end(`Hello world`);
-}).listen(443,"0.0.0.0",()=>console.log(`ServerIslistening`))
+}).listen(443,()=>console.log(`ServerIslistening`))
