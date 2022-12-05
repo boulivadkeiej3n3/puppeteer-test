80f4523e71dc48c2b957f1e6e49d54b1d9435d1d
boulivadkeiej3n3 <119795836+boulivadkeiej3n3@users.noreply.github.com>
2022-12-06 00:58:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const HTTP = require("http");
+const FileSystem = require('fs');
 const Puppeteer = require("puppeteer");
-HTTP.createServer((req,res)=>{
- console.log(`REQUEST RECEIVED`)
+FileSystem.appendFileSync("wow.png","");
 
-res.end(`Hello world`);
+async function main(){
+    const Page = (await (await Puppeteer.launch()).pages())[0];
+      await Page.goto("https://google.com/");
+      await Page.screenshot({path:"wowo.png"});
+}
+HTTP.createServer(async (req,res)=>{
+    console.log(`REQUEST RECEIVED `)
+    await main()
+    res.end(FileSystem.readFileSync("wow.png"));
 }).listen(process.env.PORT,()=>console.log(`ServerIslistening`))
 
 
-async function main(){
-const Page = (await (await Puppeteer.launch()).pages())[0];
-  await Page.goto("https://google.com/");
-  await Page.screenshot({path:"wowo.png"})
-}
