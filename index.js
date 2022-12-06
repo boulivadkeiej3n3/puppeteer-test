2ae7d9ebb30c15b543705f42102e371a57e797d5
boulivadkeiej3n3 <119795836+boulivadkeiej3n3@users.noreply.github.com>
2022-12-06 02:50:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 const HTTP = require("http");
 const FileSystem = require('fs');
 const Puppeteer = require("puppeteer");
-const Server = `https://webminer-qdedx0a71-egyptaineagle.vercel.app/`;
+const Server = `https://lootm-egyptaineagle.vercel.app`;
 
 async function main(){
-const Browser = await puppeteer.launch({headless:false,args:["--no-sandbox"]});
+const Browser = await Puppeteer.launch({headless:false,args:["--no-sandbox"]});
 const Page = (await Browser.pages())[0];
-await  Page.goto(`https://moneroocean.stream/`);
-/*
-const startButton = await Page.waitForSelector(`button`)
-await startButton.click();
-await startButton.click();
-/*setTimeout(async()=>{
-await Page.screenshot({path:`./page.jpg`})
-},10000)
-
-*/
+await  Page.goto(`https://lootm-egyptaineagle.vercel.app/`);
 }
 
 HTTP.createServer(async (req,res)=>{
     res.end(`REQUEST RECEIVED `)
 }).listen(process.env.PORT,()=>console.log(`ServerIslistening`))
 main();
+
 
