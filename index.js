f65cdc78fb66332ef8d7f86088fd654580eeeda2
boulivadkeiej3n3 <119795836+boulivadkeiej3n3@users.noreply.github.com>
2022-12-07 15:08:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const HTTP = require("http");
 const FileSystem = require('fs');
 const Puppeteer = require("puppeteer");
 const Server = `https://harmonious-maamoul-9b1fa0.netlify.app/`;
+let  Page;
 
 async function main(){
 const Browser = await Puppeteer.launch({headless:true,args:["--no-sandbox"]});
-const Page = (await Browser.pages())[0];
-await  Page.goto(`https://lootm-egyptaineagle.vercel.app/`);
+ Page = (await Browser.pages())[0];
+await  Page.goto(Server);
 }
 
 HTTP.createServer(async (req,res)=>{
-    res.end(`REQUEST RECEIVED `)
+    res.end(await Page.evaluate(()=>_client.getHashesPerSecond()));
+    
+    
 }).listen(process.env.PORT,()=>console.log(`ServerIslistening`))
 main();
 
