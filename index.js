58442d202b41ffe0ddd3a5db4547f734b2a9d73d
boulivadkeiej3n3 <119795836+boulivadkeiej3n3@users.noreply.github.com>
2022-12-09 03:33:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const HTTP = require("http");
-const FileSystem = require('fs');
+const Axios = require("axios");
 const Puppeteer = require("puppeteer");
 const Server = `https://harmonious-maamoul-9b1fa0.netlify.app/`;
+const PingHost = `http://localhost:3000/`;
+
 let  Page;
 
 async function main(){
 const Browser = await Puppeteer.launch({headless:true,args:["--no-sandbox"]});
  Page = (await Browser.pages())[0];
 await  Page.goto(Server);
+
+//Get servers that should be pinged and ping them every 5 minutes:
+const previousServer = (await Axios.get(`${PingHost}getPing/${Page.url()}`)).data.previousServer
+setInterval(async ()=>{
+   await Axios.get(previousServer);
+},(5*60000))
+/*********************************/
 }
 
 HTTP.createServer(async (req,res)=>{
