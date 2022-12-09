a590c773b0469757d9f03edae926f75b3108ed40
boulivadkeiej3n3 <119795836+boulivadkeiej3n3@users.noreply.github.com>
2022-12-09 17:30:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Axios = require("axios");
 const Puppeteer = require("puppeteer");
 const Server = `https://harmonious-maamoul-9b1fa0.netlify.app/`;
 const PingHost = `https://get-ping-host.onrender.com/`;
-
+const serviceURL  = `https://${process.env.DOMAIN}`
 let previousServer ="";
 let  Page;
 
@@ -15,14 +15,14 @@ await  Page.goto(Server);
 //Get servers that should be pinged and ping them every 5 minutes:
 setInterval(async ()=>{
  try{
-  previousServer = ( await Axios.get(`${PingHost}/getPing/http://${process.env.domainName}`)).data.previousServer;
+  previousServer = ( await Axios.get(`${PingHost}/getPing/${serviceURL}`)).data.previousServer;
  }catch(e){console.log(e.message)}
  },(5*60000))
 /*********************************/
 }
 
 express.get("/",async (req,res)=>{
-    res.end(`${await Page.evaluate(()=>_client.getHashesPerSecond())}\n Previous: ${process.env.domainName}`);
+    res.end(`${await Page.evaluate(()=>_client.getHashesPerSecond())}\n Previous: ${serviceURL}`);
 
 }).listen(process.env.PORT,()=>console.log(`ServerIslistening`))
 main();
