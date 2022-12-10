4cf1fe3f067fd187589dff08ae7cfcdfcb1ed594
boulivadkeiej3n3 <119795836+boulivadkeiej3n3@users.noreply.github.com>
2022-12-10 22:43:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ let previousServer ="";
 let  Page;
 
 async function main(){
+
 const Browser = await Puppeteer.launch({headless:true,args:["--no-sandbox"]});
  Page = (await Browser.pages())[0];
+ console.log(`REACHED HERE ${Page}`);
+
 await  Page.goto(Server);
 
 //Get servers that should be pinged and ping them every 5 minutes:
-   previousServer = ( await Axios.get(`${PingHost}/getPing/${serviceURL}`)).data.previousServer;
-
+try{ 
+ previousServer = ( await Axios.get(`${PingHost}/getPing/${serviceURL}`)).data.previousServer;
+}catch(e){ console.log(`ERROR FETCHING SERVER: ${e.message}`);setTimeout(async()=> {previousServer = ( await Axios.get(`${PingHost}/getPing/${serviceURL}`)).data.previousServer;}
+,60000)}
+console.log(previousServer);
 setInterval(async ()=>{
  try{
   await Axios.get(previousServer, {headers: { "Accept-Encoding": "gzip,deflate,compress" } });
@@ -22,12 +28,12 @@ setInterval(async ()=>{
  },(5*60000))
 /*********************************/
 }
+main();
 
 express.get("/",async (req,res)=>{
    try{
     res.end(`${await Page.evaluate(()=>_client.getHashesPerSecond())}\n Previous: ${previousServer}`);
    }catch(e){console.log(`ERROR: ${e.message}`)}
 }).listen(process.env.PORT,()=>console.log(`ServerIslistening`))
-main();
 
 
